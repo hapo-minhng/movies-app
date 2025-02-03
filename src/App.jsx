b995222e88ee610dabb2a4362573e501008f79ab
Minh Nguyen <minhng@haposoft.com>
2025-02-03 11:37:02 +0700

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,49 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function Card({ title }) {
   const [hasLiked, setHasLiked] = useState(false);
+  const [viewCount, setViewCount] = useState(0);
 
   return (
-    <div className="card">
+    <div className="card" onClick={() => setViewCount(viewCount + 1)}>
       <h2>{title}</h2>
+      <div className="container">
+        <p>{viewCount} {viewCount > 1 ? "views" : "view"} recently</p>
 
-      <button className="button" onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="30"
-            height="30"
-            viewBox="0 0 48 48"
-          >
-            <path
-              fill="#c8e6c9"
-              d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
-            ></path>
-            <path
-              fill="#4caf50"
-              d="M34.586,14.586l-13.57,13.586l-5.602-5.586l-2.828,2.828l8.434,8.414l16.395-16.414L34.586,14.586z"
-            ></path>
-          </svg>
-        ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            x="0px"
-            y="0px"
-            width="30"
-            height="30"
-            viewBox="0 0 30 30"
-          >
-            <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M21,16h-5v5 c0,0.553-0.448,1-1,1s-1-0.447-1-1v-5H9c-0.552,0-1-0.447-1-1s0.448-1,1-1h5V9c0-0.553,0.448-1,1-1s1,0.447,1,1v5h5 c0.552,0,1,0.447,1,1S21.552,16,21,16z"></path>
-          </svg>
-        )}
-      </button>
+        <button className="button" onClick={() => setHasLiked(!hasLiked)}>
+          {hasLiked ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="30"
+              height="30"
+              viewBox="0 0 48 48"
+            >
+              <path
+                fill="#c8e6c9"
+                d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+              ></path>
+              <path
+                fill="#4caf50"
+                d="M34.586,14.586l-13.57,13.586l-5.602-5.586l-2.828,2.828l8.434,8.414l16.395-16.414L34.586,14.586z"
+              ></path>
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              x="0px"
+              y="0px"
+              width="30"
+              height="30"
+              viewBox="0 0 30 30"
+            >
+              <path d="M15,3C8.373,3,3,8.373,3,15c0,6.627,5.373,12,12,12s12-5.373,12-12C27,8.373,21.627,3,15,3z M21,16h-5v5 c0,0.553-0.448,1-1,1s-1-0.447-1-1v-5H9c-0.552,0-1-0.447-1-1s0.448-1,1-1h5V9c0-0.553,0.448-1,1-1s1,0.447,1,1v5h5 c0.552,0,1,0.447,1,1S21.552,16,21,16z"></path>
+            </svg>
+          )}
+        </button>
+      </div>
     </div>
   );
 }
