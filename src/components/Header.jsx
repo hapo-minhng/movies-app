23592ebc22280cae8c4ce094bc4f5140e6317500
Minh Nguyen <minhng@haposoft.com>
2025-02-03 13:46:24 +0700

    Restore project. Add new layout and header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="./hero.png" alt="Hero Banner" />
+
+            <h1>Find <span className="text-gradient">Movies</span> You'll Love Without the Hassle</h1>
+        </header>
+    )
+}
