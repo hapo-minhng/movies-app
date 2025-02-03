e8034e5e4fbe313fd49ce2ff742057fbcaeb717b
Minh Nguyen <minhng@haposoft.com>
2025-02-03 14:09:15 +0700

    Add search logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Search from "./components/Search";
 
 export default function App() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <main>
       <div className="pattern" />
+
       <div className="wrapper">
         <Header />
 
-        <Search />
+        <Search searchText={searchText} setSearchText={setSearchText} />
       </div>
     </main>
   );
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,15 @@
-export default function Search() {
+export default function Search({ searchText, setSearchText }) {
   return (
     <div className="search">
       <div>
         <img src="./search.svg" alt="search" />
 
-        <input type="text" placeholder="Search through 300+ movies online" />
+        <input
+          type="text"
+          placeholder="Search through 300+ movies online"
+          value={searchText}
+          onChange={(s) => setSearchText(s.target.value)}
+        />
       </div>
     </div>
   );
